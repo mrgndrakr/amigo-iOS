5bc9e2aa6f4b09abe47b96aa16ebb805129e1a16
mrgndrakr <mrigendra@Mrigendras-MacBook-Pro.local>
2014-08-31 11:13:28 +0530

    Initial Commit

---
+++ migiFirst/SMViewController.h
@@ -0,0 +1,13 @@
+//
+//  SMViewController.h
+//  migiFirst
+//
+//  Created by Mrigendra Kumar on 31/08/14.
+//  Copyright (c) 2014 tripOntech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SMViewController : UIViewController
+
+@end
---
+++ migiFirst/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  migiFirst
+//
+//  Created by Mrigendra Kumar on 31/08/14.
+//  Copyright (c) 2014 tripOntech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "SMAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([SMAppDelegate class]));
+    }
+}
