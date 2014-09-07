ac8bf49323ddc2d31e13630a63ed33d447cca59d
Mrigendra Kumar <mrgndrakr@live.com>
2014-09-08 01:49:11 +0530

    Amigo Design Phase 1

--- migiFirst/SMViewController.h
+++ migiFirst/SMViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface SMViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *predictLabel;
+- (IBAction)btnPressed;
 
 @end
--- migiFirst/SMViewController.m
+++ migiFirst/SMViewController.m
@@ -26,4 +26,7 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)btnPressed {
+    self.predictLabel.text = @"Button Pressed";
+}
 @end
